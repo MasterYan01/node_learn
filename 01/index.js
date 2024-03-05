ef7bfd763bbdbd64ceb5b8e46264567385525777
Ethan <ethanyan@castec.com.tw>
2024-03-05 17:02:28 +0800

    測試20240305

---
+++ 01/index.js
@@ -0,0 +1,11 @@
+// import dayjs from 'dayjs';
+const dayjs = require('dayjs');
+
+// import obj from './utils.js'
+// const obj = require('./utils')
+// console.log(getArraySum)
+// console.log(getArraySum([1,2,3,4,5,6]))
+// const result = obj.arraySum([5,1,2,3])
+// console.log(result)
+const newDayjs = dayjs().format('YYYY-MM-DD');
+console.log(newDayjs);
---
+++ 01/utils.js
@@ -0,0 +1,8 @@
+export const getArraySum = arr => arr.reduce((sum, item) => sum += item, 0)
+// module.exports={
+//   arraySum:getArraySum
+// }
+//默認導出
+// export default{
+//   arraySum:getArraySum
+// }
