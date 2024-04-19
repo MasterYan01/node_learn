6c81e74b62bb05df487d76c2bf0166f5c3f2ea7c
Ethan <ethanyan@castec.com.tw>
2024-04-19 17:09:18 +0800

    4/19 pm0509 updated

--- 20240419/server.js
+++ 20240419/server.js
@@ -1,9 +1,11 @@
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
+//同源插件cors 解決跨域問題
 //const cors = require('cors')
 const server = express()
 //server.use(cors())
+//靜態資源綁定
 server.use(express.static('./public'))
 
 server.get('/api/city',(req,res)=>{
